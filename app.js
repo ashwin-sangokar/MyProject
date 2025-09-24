a4f44c3f702cbcbe3da7760a9f7ff94147c1bdbf
ashwin-sangokar <149792398+ashwin-sangokar@users.noreply.github.com>
2025-09-24 22:52:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
 const store = MongoStore.create({
-    mongoUrl: "mongodb://127.0.0.1:27017/WanderLust",
+    mongoUrl: dbUrl,
     crypto: {
         secret: process.env.SECRET 
     }
